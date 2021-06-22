2e3262aab801abf02b27e4dc80c25474a5ca6ce5
deepthi <deepthi.nanjunda@gmail.com>
2021-06-23 01:15:05 +0530

    formatting changed

--- src/app/model/flashcard.java
+++ src/app/model/flashcard.java
@@ -1,42 +1,41 @@
 package app.model;
 
-public class flashcard
-{
-    private int cardno;
-    private String type;
-    private String word;
-    private String meaning;
+public class flashcard {
+  private int cardno;
+  private String type;
+  private String word;
+  private String meaning;
 
 
-    public int getCardno() {
-        return cardno;
-    }
+  public int getCardno() {
+    return cardno;
+  }
 
-    public void setCardno(int cardno) {
-        this.cardno = cardno;
-    }
+  public void setCardno(int cardno) {
+    this.cardno = cardno;
+  }
 
-    public String getType() {
-        return type;
-    }
+  public String getType() {
+    return type;
+  }
 
-    public void setType(String type) {
-        this.type = type;
-    }
+  public void setType(String type) {
+    this.type = type;
+  }
 
-    public String getWord() {
-        return word;
-    }
+  public String getWord() {
+    return word;
+  }
 
-    public void setWord(String word) {
-        this.word = word;
-    }
+  public void setWord(String word) {
+    this.word = word;
+  }
 
-    public String getMeaning() {
-        return meaning;
-    }
+  public String getMeaning() {
+    return meaning;
+  }
 
-    public void setMeaning(String meaning) {
-        this.meaning = meaning;
-    }
+  public void setMeaning(String meaning) {
+    this.meaning = meaning;
+  }
 }
--- src/app/ui/screen3.java
+++ src/app/ui/screen3.java
@@ -2,17 +2,15 @@ package app.ui;
 
 import java.sql.Connection;
 
-public class screen3
-{
-    private Connection connection;
-    screen3(Connection connection)
-    {
-        this.connection=connection;
-    }
+public class screen3 {
+  private final Connection connection;
 
-    void run()
-    {
+  screen3(Connection connection) {
+    this.connection = connection;
+  }
 
-    }
+  void run() {
+
+  }
 
 }

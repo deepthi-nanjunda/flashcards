2c56d2c4d72a30702a2b64557324a1645baa12c4
deepthi <deepthi.nanjunda@gmail.com>
2021-06-30 22:26:37 +0530

    supplier added popup deletion button added and working

--- src/app/component/DisplayCard.java
+++ src/app/component/DisplayCard.java
@@ -4,44 +4,39 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.function.Supplier;
 
 public class DisplayCard extends JPanel implements ActionListener {
 
 
-  private String word, meaning;
-  private boolean displayWord;
-  private JLabel jLabel;
+  private String meaning;
   private JButton jButton;
+  private JTextArea jTextArea;
+  private JPanel basePanel;
+  private Popup popup;
+  private Supplier<Boolean> myFunc;
 
-  public DisplayCard(String word,
-                     String meaning,
-                     boolean displayWord) {
-    this.word = word;
+  public DisplayCard(final Supplier<Boolean> myFunc,
+                     final String meaning) {
     this.meaning = meaning;
-    this.displayWord = displayWord;
-
-    if (displayWord) {
-      this.jLabel = new JLabel(word);
-    } else {
-      this.jLabel = new JLabel(meaning);
-    }
+    this.basePanel = basePanel;
+    this.jTextArea = new JTextArea(this.meaning);
+    this.myFunc = myFunc;
     jButton = new JButton("Click me");
     jButton.addActionListener(this);
 
+    setVisible(true);
     setBounds(150, 180, 300, 400);
     setBackground(Color.ORANGE);
     setLayout(new GridLayout());
-    add(jLabel);
+    add(jTextArea);
     add(jButton);
   }
 
-
   @Override
   public void actionPerformed(ActionEvent e) {
-  if(!displayWord)
-  {
-    jLabel.setVisible(false);
+
+    myFunc.get();
   }
 
-  }
 }

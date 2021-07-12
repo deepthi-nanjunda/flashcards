1ddfc793475197a3737c823a61b5dae7dc0cac12
deepthi <deepthi.nanjunda@gmail.com>
2021-07-12 23:50:28 +0530

    added another tab in the tabbed pane called the insertScreen

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,8 @@ package app;
 
 import app.ui.Screen1;
 import app.ui.Screen2;
+import app.ui.insertScreen;
+import app.userMadeException.customException;
 
 import javax.swing.*;
 import java.sql.Connection;
@@ -13,15 +15,19 @@ public class Main {
   public static void main(String[] args) throws SQLException {
     // write your code here
 
-    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/flash_card", "root", "MyLibraSql@123");
-    JFrame jframe = new JFrame();
-    jframe.setVisible(true);
-    jframe.setSize(500,600);
-    JTabbedPane jTabbedPane = new JTabbedPane(JTabbedPane.TOP);
-    Screen2 screen2 = new Screen2(connection);
-    jTabbedPane.add("type", new Screen1(connection,screen2, jTabbedPane));
-    jTabbedPane.add("words",screen2);
+      Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/flash_card", "root", "MyLibraSql@123");
+      JFrame jframe = new JFrame();
+      jframe.setVisible(true);
+      jframe.setSize(500, 600);
+      jframe.setLocationRelativeTo(null);
+      JTabbedPane jTabbedPane = new JTabbedPane(JTabbedPane.TOP);
+      Screen2 screen2 = new Screen2(connection);
+      insertScreen insertScreen = new insertScreen(connection);
+      jTabbedPane.add("type", new Screen1(connection, screen2, jTabbedPane));
+      jTabbedPane.add("words", screen2);
+      jTabbedPane.add("insertScreen", insertScreen);
+      jframe.add(jTabbedPane);
 
-    jframe.add(jTabbedPane);
+
   }
 }

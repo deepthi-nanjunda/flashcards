8fcd29d0652c0c0c4eee94a01d7986a592c1639e
deepthi <deepthi.nanjunda@gmail.com>
2021-07-17 13:02:40 +0530

    layout changing

--- src/app/ui/insert.java
+++ src/app/ui/insert.java
@@ -13,14 +13,14 @@ import java.sql.Statement;
 import java.util.function.Supplier;
 
 
-public class insertScreen extends JPanel implements ActionListener {
+public class insert extends JPanel implements ActionListener {
   private final JLabel type;
   private final JLabel title;
-  private final JTextField typeName;
+  private final JTextArea typeName;
   private final JLabel word;
-  private final JTextField wordName;
+  private final JTextArea wordName;
   private final JLabel meaning;
-  private final JTextField explain;
+  private final JTextArea explain;
   private final JButton submit;
   private final Connection connection;
   private PopupFactory popupFactory;
@@ -28,48 +28,68 @@ public class insertScreen extends JPanel implements ActionListener {
   private Supplier<Boolean> myFunc;
   private final JButton reset;
 
-  public insertScreen(Connection connection) throws SQLException {
+  public insert(Connection connection) throws SQLException {
 
     this.connection = connection;
-    title = new JLabel("Insertion into the data base");
+    setLayout(new FlowLayout());
+
+    JPanel panel = new JPanel();
+    panel.setSize(300,300);
+    BorderLayout layout = new BorderLayout();
+    layout.setHgap(20);
+    layout.setVgap(20);
+    panel.setLayout(layout);
+
+    title = new JLabel();
+    title.setText("Insertion");
     title.setSize(100, 30);
     add(title);
-    setLayout(new GridLayout(0, 2));
-
+   // setLayout(new GridLayout(0, 2));
 
     type = new JLabel("Type");
-    type.setSize(20, 20);
-    add(type);
+    type.setSize(20,20);
+   // type.setLocation(100,100);
+   // type.setSize(20, 20);
+    panel.add(type,BorderLayout.LINE_START);
 
-    typeName = new JTextField();
-    typeName.setSize(50, 20);
-    add(typeName);
+    typeName = new JTextArea();
+    typeName.setSize(50,20);
+    //typeName.setLocation(150, 100);
+    panel.add(typeName, BorderLayout.EAST);
 
     word = new JLabel("word");
-    word.setSize(20, 20);
-    add(word);
+    word.setSize(20,20);
+    //word.setLocation(100, 170);
+    panel.add(word,BorderLayout.WEST);
 
-    wordName = new JTextField();
-    wordName.setSize(70, 20);
-    add(wordName);
+    wordName = new JTextArea();
+    wordName.setSize(70,20);
+//    wordName.setLocation(150, 770);
+    panel.add(wordName,BorderLayout.EAST);
 
     meaning = new JLabel("meaning");
-    meaning.setSize(70, 20);
-    add(meaning);
+    meaning.setSize(70,20);
+//    meaning.setLocation(100, 240);
+    panel.add(meaning,BorderLayout.WEST);
 
-    explain = new JTextField();
-    explain.setSize(70, 20);
-    add(explain);
+    explain = new JTextArea();
+    explain.setSize(70,20);
+//    explain.setLocation(150, 240);
+    panel.add(explain, BorderLayout.EAST);
 
     submit = new JButton("submit");
-    submit.setSize(20, 20);
+    submit.setSize(20,20);
+//    submit.setLocation(300, 300);
     submit.addActionListener(this);
-    add(submit);
+    add(submit,BorderLayout.SOUTH);
 
     reset = new JButton("Reset");
     reset.setSize(20,20);
+    //reset.setLocation(300,300);
     reset.addActionListener(this);
-    add(reset);
+    panel.add(reset, BorderLayout.SOUTH);
+
+    add(panel);
 
   }
 
